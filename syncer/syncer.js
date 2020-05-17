ee4f93c080250db10dffd0c1c5835c9171d5fc62
Danny Diekroeger <dannydiek@gmail.com>
2020-05-17 15:23:51 -0700

    keep socket alive

--- syncer/syncer.js
+++ syncer/syncer.js
@@ -4,7 +4,6 @@ const jspath = '../priv/static/js'
 const messagesFile = `${jspath}/messages.js`
 let messages = JSON.parse(fs.readFileSync(messagesFile).toString().replace('const messages = ', ''))
 const util = require(`${jspath}/message-util.js`)
-const ws = new WebSocket('wss://localhost/ws/chat');
 
 function localMaybeSaveMessage(message) {
     if (util.isIllegal(message)) return false;
@@ -17,18 +16,23 @@ function localMaybeSaveMessage(message) {
     true;
 }
 
-ws.on('open', () => {
-    console.log('Socket opened')
-    ws.on('message', function incoming(data) {
-        localMaybeSaveMessage(data)
-    });
-    
-    ws.on('close', () => {
-        console.log('socket closed')
+function begin() {
+    const ws = new WebSocket('wss://localhost/ws/chat');
+    ws.on('open', () => {
+        console.log('Socket opened')
+        ws.on('message', function incoming(data) {
+            localMaybeSaveMessage(data)
+        });
+        
+        ws.on('close', () => {
+            console.log('socket closed')
+            setTimeout(begin, 1)
+        })
+        setInterval(() => {
+            const data = `const messages = ${JSON.stringify(messages)}`
+            fs.writeFileSync(`${jspath}/messages.js`, data)
+        }, 1000)
     })
-    setInterval(() => {
-        const data = `const messages = ${JSON.stringify(messages)}`
-        fs.writeFileSync(`${jspath}/messages.js`, data)
-    }, 1000)
-})
+}
 
+begin()
