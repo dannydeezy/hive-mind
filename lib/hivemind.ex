d0a699d7069e2a66d4988c14c1bbadf6560eecf0
Danny Diekroeger <dannydiek@gmail.com>
2020-05-17 11:32:20 -0700

    Add https config

--- lib/hivemind.ex
+++ lib/hivemind.ex
@@ -2,20 +2,17 @@ defmodule Hivemind do
   use Application
 
   def start(_type, _args) do
-    cowboy_options = [
-      keyfile: "/etc/letsencrypt/live/hive-mind.xyz/privkey.pem",
-      certfile: "/etc/letsencrypt/live/hive-mind.xyz/fullchain.pem",
-      opt_app: :secure_app
-    ]
     children = [
       Plug.Cowboy.child_spec(
         scheme: :https,
         plug: Hivemind.Router,
         options: [
           dispatch: dispatch(),
-          port: 443
-        ],
-        cowboy_options
+          port: 443,
+          keyfile: "/etc/letsencrypt/live/hive-mind.xyz/privkey.pem",
+          certfile: "/etc/letsencrypt/live/hive-mind.xyz/fullchain.pem",
+          opt_app: :secure_app
+        ]
       ),
       Registry.child_spec(
         keys: :duplicate,
